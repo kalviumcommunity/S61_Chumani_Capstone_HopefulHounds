4e2198d3328308f3837eb89270e941330082255e
Chumani Lalasa <chumanilalasa@gmail.com>
2024-05-15 18:30:53 +0530

    components created

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./Config/dbconn');
 const app = express();
 const dogRoute = require('./Routes/DogDetailsRoutes');
 const accessoriesRoute = require('./Routes/accessoriesRoutes');
 const hospitalsRoute = require('./Routes/hospitalsRoutes');
 app.use(express.json());
+app.use(cors());
 connectDB();
 app.get('/', (req, res) => {
     res.send('Get request through express')
