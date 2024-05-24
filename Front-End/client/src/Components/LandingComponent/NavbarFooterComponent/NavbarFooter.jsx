4d6f9fd54f0cabb255d6d82de3e099629331a997
Chumani Lalasa <chumanilalasa@gmail.com>
2024-05-24 15:08:29 +0530

    added login component

--- Front-End/client/src/Components/LandingComponent/NavbarFooterComponent/NavbarFooter.jsx
+++ Front-End/client/src/Components/LandingComponent/NavbarFooterComponent/NavbarFooter.jsx
@@ -20,12 +20,18 @@ function NavbarFooter({children}) {
         </div>
         
         <div className="nav-options">
+        <Link to="/adopt">
+            <h2>Adopt</h2>
+          </Link>
           <Link to="/shop">
             <h2>Shop</h2>
           </Link>
 
           <Link to="/about">
             <h2>About</h2>
+          </Link>
+          <Link to="/login">
+            <h2 style={{border:"4px solid black", padding:"7px", borderRadius:"20px", backgroundColor:"#7e0081", color:"white", position:"relative", bottom:"10px"}}>Login</h2>
           </Link>
         </div>
       </div>
